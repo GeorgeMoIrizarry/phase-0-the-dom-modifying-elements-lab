ec87500f3550378d0226d3e2447ec434c2c8e5ef
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-03-13 22:38:40 -0400

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const mainDelete = document.getElementById("main")
+main.remove(mainDelete)
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = "George is the champion"
